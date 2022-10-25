f1555e34e8a77917eaa1a8ac61844e1aac01357b
JKrecisz <jakub.krecisz@icloud.com>
2022-10-25 23:04:03 +0200

    NUM2

--- NUM2/main.py
+++ NUM2/main.py
@@ -1,21 +1,28 @@
 import numpy as np
 
-A1 = np.array([
-    [2.34332898, -0.11253278,  -0.01485349, 0.33316649,  0.71319625],
-    [-0.11253278, 1.67773628, -0.32678856, -0.31118836,  -0.43342631],
-    [-0.01485349, -0.32678856,  2.66011353, 0.85462464, 0.16698798],
-    [0.33316649, -0.31118836, 0.85462464, 1.54788582, 0.32269197],
-    [0.71319625, -0.43342631, 0.16698798, 0.32269197, 3.27093538]],
-    dtype=np.float64)
+from config import A1, A2, B
 
-A2 = np.array([
-    [2.34065520, -0.05353743, 0.00237792, 0.32944082, 0.72776588],
-    [-0.05353743, 0.37604149, -0.70698859, -0.22898376, -0.75489595],
-    [0.00237792, -0.70698859, 2.54906441, 0.87863502, 0.07309288],
-    [0.32944082, -0.22898376, 0.87863502, 1.54269444, 0.34299341],
-    [0.72776588, -0.75489595, 0.07309288, 0.34299341, 3.19154447]],
-    dtype=np.float64)
+if __name__ == '__main__':
+    Bp = B + np.array([10 ** -5, 0, 0, 0, 0]).transpose()
 
-B = np.array(
-    [3.55652063354463, -1.86337418741501, 5.84125684808554, -1.74587299057388, 0.84299677124244],
-    dtype=np.float64)
+    y1 = np.linalg.solve(A1, B)
+    y2 = np.linalg.solve(A2, B)
+    yp1 = np.linalg.solve(A1, Bp)
+    yp2 = np.linalg.solve(A2, Bp)
+
+    print("y1: ", np.linalg.solve(A1, B))
+    print("y2: ", np.linalg.solve(A2, B))
+
+    print("y'1: ", np.linalg.solve(A1, Bp))
+    print("y'2: ", np.linalg.solve(A2, Bp))
+
+    d1 = np.linalg.norm(y1 - yp1)
+    d2 = np.linalg.norm(y2 - yp2)
+
+    print("delta1: {:.20f}".format(d1))
+    print("delta2: {:.20f}".format(d2))
+    print('\n')
+
+    # Dodatkowy kod do wniosku
+    print("Wspolczynnik uwarunkowania macierzy A1: ", np.linalg.cond(A1))
+    print("Wspolczynnik uwarunkowania macierzy A2: ", np.linalg.cond(A2))
