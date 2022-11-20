c4a2225b7c62f1c662321a080cdd86712ebdc0e7
JKrecisz <jakub.krecisz@icloud.com>
2022-11-20 19:16:20 +0100

    NUM5 done

--- NUM5/main.py
+++ NUM5/main.py
@@ -4,7 +4,17 @@ import matplotlib.pyplot as plt
 from config import N, B_VECTOR, X_VECTOR, MAX_NUM_OF_ITERATIONS, PRECISION
 
 
-def solve_by_jacobin_method(xVector, max_iterations, size, bVector):
+def solve_by_jacobin_method(xVector: list, bVector: list, size: int, max_iterations: int) -> list:
+    """
+    The function determines the solution of our given equation to the
+    specified precision of the results or to the maximum number of iterations by Jacobin method.
+
+    :param xVector: our vector x with example values
+    :param bVector: our b vector from the equation
+    :param size: our matrix dimension
+    :param max_iterations: maximum number of iterations
+    :return: list of approximations for all iterations
+    """
     iterativeApprox = []
     previousNorm = 0
     for _ in range(max_iterations):
@@ -24,8 +34,10 @@ def solve_by_jacobin_method(xVector, max_iterations, size, bVector):
                 xVector[index] = (bVector[index] - copyOfxVector[index - 1] - 0.2 * copyOfxVector[index - 2] -
                                   xVector[index + 1] - 0.2 * xVector[index + 2]) / 3
 
+        # checking the norm for actual iteration
         actualNorm = np.sqrt(sum(map(lambda a, b: (a - b) ** 2, xVector, copyOfxVector)))
 
+        # saving approximation of actual iteration
         iterativeApprox.append(xVector.copy())
 
         # convergence check
@@ -37,7 +49,17 @@ def solve_by_jacobin_method(xVector, max_iterations, size, bVector):
     return iterativeApprox
 
 
-def solve_by_gauss_seidel_method(xVector, max_iterations, size, bVector):
+def solve_by_gauss_seidel_method(xVector: list, bVector: list, size: int, max_iterations: int) -> list:
+    """
+    The function determines the solution of our given equation to the
+    specified precision of the results or to the maximum number of iterations by Gauss-Seidel method.
+
+    :param xVector: our vector x with example values
+    :param bVector: our b vector from the equation
+    :param size: our matrix dimension
+    :param max_iterations: maximum number of iterations
+    :return: list of approximations for all iterations
+    """
     iterativeApprox = []
     previousNorm = 0
     for _ in range(max_iterations):
@@ -60,6 +82,7 @@ def solve_by_gauss_seidel_method(xVector, max_iterations, size, bVector):
         # checking the norm for actual iteration
         actualNorm = np.sqrt(sum(map(lambda a, b: (a - b) ** 2, xVector, copyOfxVector)))
 
+        # saving approximation of actual iteration
         iterativeApprox.append(xVector.copy())
 
         # convergence check
@@ -71,7 +94,14 @@ def solve_by_gauss_seidel_method(xVector, max_iterations, size, bVector):
     return iterativeApprox
 
 
-def generate_graph(firstDiffs, secondDiffs, titleStr):
+def generate_graph(firstDiffs: list, secondDiffs: list, titleStr: str):
+    """
+    The function generates a graph based on the differences and their iteration number.
+
+    :param firstDiffs: first difference list
+    :param secondDiffs: second difference list
+    :param titleStr: title of our graph
+    """
     plt.grid(True)
     plt.xlabel('Number of iterations (n)')
     plt.ylabel("$|x(n) - x[-1]|$")
@@ -85,8 +115,8 @@ def generate_graph(firstDiffs, secondDiffs, titleStr):
 
 if __name__ == '__main__':
     for title, xVect in X_VECTOR.items():
-        iterativeApproxJacobi = solve_by_jacobin_method(xVect.copy(), MAX_NUM_OF_ITERATIONS, N, B_VECTOR)
-        iterativeApproxGaussSeidel = solve_by_gauss_seidel_method(xVect.copy(), MAX_NUM_OF_ITERATIONS, N, B_VECTOR)
+        iterativeApproxJacobi = solve_by_jacobin_method(xVect.copy(), B_VECTOR, N, MAX_NUM_OF_ITERATIONS)
+        iterativeApproxGaussSeidel = solve_by_gauss_seidel_method(xVect.copy(), B_VECTOR, N, MAX_NUM_OF_ITERATIONS)
 
         # Last iteration is our solution in that case
         print(f'Solution by Jacobi method:\n{iterativeApproxJacobi[-1]}')
@@ -101,5 +131,5 @@ if __name__ == '__main__':
             diffsGaussSeidel.append(
                 np.sqrt(sum(map(lambda a, b: (a - b) ** 2, approximateSolution, iterativeApproxGaussSeidel[-1]))))
 
-        # Generate the graph
+        # Generate a graph
         generate_graph(diffsJacobi, diffsGaussSeidel, titleStr=title)
