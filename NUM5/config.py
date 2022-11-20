c98d00983536d1a46c9a4b515b0e75c7924e092b
JKrecisz <jakub.krecisz@icloud.com>
2022-11-20 18:53:51 +0100

    NUM5

--- NUM5/config.py
+++ NUM5/config.py
@@ -4,9 +4,9 @@ N = 100
 
 B_VECTOR = [x for x in range(1, N + 1)]
 
-X_VECTOR = {'RANDOM': random.sample(range(1, 500), N),
-            'ZEROS': [0 for _ in range(N)],
-            'TENS': [10 for _ in range(N)]}
+X_VECTOR = {'random': random.sample(range(1, 500), N),
+            'zeros': [0 for _ in range(N)],
+            'tens': [10 for _ in range(N)]}
 
 MAX_NUM_OF_ITERATIONS = 500
 
--- NUM5/main.py
+++ NUM5/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from config import N, B_VECTOR, X_VECTOR, MAX_NUM_OF_ITERATIONS, PRECISION
 
 
-def solve_by_jacobian_method(xVector, max_iterations, size, bVector):
+def solve_by_jacobin_method(xVector, max_iterations, size, bVector):
     iterativeApprox = []
     previousNorm = 0
     for _ in range(max_iterations):
@@ -13,26 +13,28 @@ def solve_by_jacobian_method(xVector, max_iterations, size, bVector):
             if index == 0:
                 xVector[index] = (bVector[index] - xVector[index + 1] - 0.2 * xVector[index + 2]) / 3
             elif index == 1:
-                xVector[index] = (bVector[index] - copyOfxVector[index - 1] - xVector[index + 1] - 0.2 * xVector[
-                    index + 2]) / 3
+                xVector[index] = (bVector[index] - copyOfxVector[index - 1] - xVector[index + 1] - 0.2 *
+                                  xVector[index + 2]) / 3
             elif index == size - 2:
-                xVector[index] = (bVector[index] - copyOfxVector[index - 1] - 0.2 * copyOfxVector[index - 2] - xVector[
-                    index + 1]) / 3
+                xVector[index] = (bVector[index] - copyOfxVector[index - 1] - 0.2 * copyOfxVector[index - 2] -
+                                  xVector[index + 1]) / 3
             elif index == size - 1:
                 xVector[index] = (bVector[index] - copyOfxVector[index - 1] - 0.2 * copyOfxVector[index - 2]) / 3
             else:
-                xVector[index] = (bVector[index] - copyOfxVector[index - 1] - 0.2 * copyOfxVector[index - 2] - xVector[
-                    index + 1] - 0.2 * xVector[index + 2]) / 3
+                xVector[index] = (bVector[index] - copyOfxVector[index - 1] - 0.2 * copyOfxVector[index - 2] -
+                                  xVector[index + 1] - 0.2 * xVector[index + 2]) / 3
+
         actualNorm = np.sqrt(sum(map(lambda a, b: (a - b) ** 2, xVector, copyOfxVector)))
 
         iterativeApprox.append(xVector.copy())
 
-        # Sprawdzenie czy zbiegło
-        if abs(previousNorm - actualNorm) < 10 ** (-12):
+        # convergence check
+        if abs(previousNorm - actualNorm) < 10 ** PRECISION:
             break
-        previousNorm = actualNorm
+        else:
+            previousNorm = actualNorm
 
-    return iterativeApprox.pop(), xVector
+    return iterativeApprox
 
 
 def solve_by_gauss_seidel_method(xVector, max_iterations, size, bVector):
@@ -52,54 +54,52 @@ def solve_by_gauss_seidel_method(xVector, max_iterations, size, bVector):
             elif index == size - 1:
                 xVector[index] = (bVector[index] - xVector[index - 1] - 0.2 * xVector[index - 2]) / 3
             else:
-                xVector[index] = (bVector[index] - xVector[index - 1] - 0.2 * xVector[index - 2] - xVector[
-                    index + 1] - 0.2 * xVector[index + 2]) / 3
+                xVector[index] = (bVector[index] - xVector[index - 1] - 0.2 * xVector[index - 2] -
+                                  xVector[index + 1] - 0.2 * xVector[index + 2]) / 3
 
+        # checking the norm for actual iteration
         actualNorm = np.sqrt(sum(map(lambda a, b: (a - b) ** 2, xVector, copyOfxVector)))
 
         iterativeApprox.append(xVector.copy())
 
-        # Sprawdzenie czy zbiegło
+        # convergence check
         if abs(previousNorm - actualNorm) < 10 ** PRECISION:
             break
+        else:
+            previousNorm = actualNorm
 
-        previousNorm = actualNorm
-
-    return iterativeApprox.pop(), xVector
+    return iterativeApprox
 
 
-def generate_graph(firstDiffs, secondDiffs, title):
+def generate_graph(firstDiffs, secondDiffs, titleStr):
     plt.grid(True)
-    plt.xlabel('n')
+    plt.xlabel('Number of iterations (n)')
     plt.ylabel("$|x(n) - x[-1]|$")
     plt.yscale('log')
     plt.plot(list(range(1, len(firstDiffs) + 1)), firstDiffs, 'tab:green')
     plt.plot(list(range(1, len(secondDiffs) + 1)), secondDiffs, 'tab:red')
-    plt.legend(['Jacobian Method', 'Gauss-Seidel method'])
-    plt.title(f'Comparison iterative methods; x with all {title} values')
+    plt.legend(['Jacobin Method', 'Gauss-Seidel method'])
+    plt.title(f'Comparison iterative methods; x with all {titleStr} values')
     plt.show()
 
 
 if __name__ == '__main__':
-    for key, xValues in X_VECTOR.items():
-        iterativeApproxJacobi, solutionJacobi = solve_by_jacobian_method(xValues.copy(),
-                                                                         MAX_NUM_OF_ITERATIONS, N,
-                                                                         B_VECTOR)
-        iterativeApproxGaussSeidel, solutionGaussSeidel = solve_by_gauss_seidel_method(xValues.copy(),
-                                                                                       MAX_NUM_OF_ITERATIONS, N,
-                                                                                       B_VECTOR)
+    for title, xVect in X_VECTOR.items():
+        iterativeApproxJacobi = solve_by_jacobin_method(xVect.copy(), MAX_NUM_OF_ITERATIONS, N, B_VECTOR)
+        iterativeApproxGaussSeidel = solve_by_gauss_seidel_method(xVect.copy(), MAX_NUM_OF_ITERATIONS, N, B_VECTOR)
 
-        print(f'Solution by Jacobi method:\n{solutionJacobi}')
-        print(f'Solution by Gauss-Seidel method:\n{solutionGaussSeidel}')
+        # Last iteration is our solution in that case
+        print(f'Solution by Jacobi method:\n{iterativeApproxJacobi[-1]}')
+        print(f'Solution by Gauss-Seidel method:\n{iterativeApproxGaussSeidel[-1]}')
 
-        # Obliczanie różnicy pomiędzy rozwiązaniem w poszczególnych iteracjach a rozwiązaniem dokładnym
-        diffsJacobi = []
+        # Calculate the difference between all iteration and the last iteration
+        diffsJacobi, diffsGaussSeidel = [], []
         for approximateSolution in iterativeApproxJacobi:
-            diffsJacobi.append(np.sqrt(sum(map(lambda a, b: (a - b) ** 2, approximateSolution, solutionJacobi))))
-
-        diffsGaussSeidel = []
+            diffsJacobi.append(
+                np.sqrt(sum(map(lambda a, b: (a - b) ** 2, approximateSolution, iterativeApproxJacobi[-1]))))
         for approximateSolution in iterativeApproxGaussSeidel:
             diffsGaussSeidel.append(
-                np.sqrt(sum(map(lambda a, b: (a - b) ** 2, approximateSolution, solutionGaussSeidel))))
+                np.sqrt(sum(map(lambda a, b: (a - b) ** 2, approximateSolution, iterativeApproxGaussSeidel[-1]))))
 
-        generate_graph(diffsJacobi, diffsGaussSeidel, title=key)
+        # Generate the graph
+        generate_graph(diffsJacobi, diffsGaussSeidel, titleStr=title)
