0c1ce7fea69a748641ff40885f9e1207a6c3055e
JKrecisz <jakub.krecisz@icloud.com>
2022-12-12 22:20:09 +0100

    NUM7 small corrections

--- NUM7/main.py
+++ NUM7/main.py
@@ -1,50 +1,48 @@
 import matplotlib.pyplot as plt
 import sys
-from config import X_NEW, PARAMS
+from config import X_RANGE, FUNCTION_PARAMS
 
-def interpolation(fun, node, arg, n):
-    x = node(n)
-    y, y_new = list(map(lambda a: fun(a), x)), []
-    for a in arg:
+def interpolation(function, nodeFunction, xArguments, degree):
+    x = nodeFunction(degree)
+    y, y_new = list(map(lambda a: function(a), x)), []
+    for xArgument in xArguments:
         val = 0
-        # Interpolacja
-        for i in range(n + 1):
-            tmp = 1
-            for k in range(n + 1):
-                if i != k:
-                    tmp = tmp * (a - x[k]) / (x[i] - x[k])
-            val = val + y[i] * tmp
+        for j in range(degree + 1):
+            product = 1
+            for k in range(degree + 1):
+                if j != k:
+                    product *= (xArgument - x[k]) / (x[j] - x[k])
+            val += y[j] * product
         y_new.append(val)
     return y_new
 
-def savePlot(arg):
-    for column, function in enumerate(PARAMS.values()):
-        for row, node in enumerate(function['nodes']):
+def save_plots(arg):
+    for column, function in enumerate(FUNCTION_PARAMS.values()):
+        for row, node in enumerate(function['interpolation_nodes']):
             plt.figure(figsize=(8, 8))
             plt.title(
                 'Wielomiany interpolacyjne dla funkcji\n' rf"{function['function_title']} i siatki {node['node_title']}")
             plt.plot(arg, function['function'](arg), 'r', label='f(x)', linewidth=2.5)
-            for degree in node['polynomial_degree']:
+            for degree in node['polynomial_degrees']:
                 plt.plot(arg, interpolation(function['function'], node['function'], arg, degree),
                          label=rf"$W_{'{' + str(degree) + '}'}(x)$")
             plt.xlabel('x')
             plt.ylabel('y')
             plt.grid()
             plt.legend()
-            plt.savefig(f"wykres{column + row}.svg")
+            plt.savefig(f"wykres_{column}-{row}.svg")
             plt.clf()
-            return True
 
-def showPlot(arg):
+def show_plots(arg):
     fig, axs = plt.subplots(2, 2)
     fig.set_figheight(6)
     fig.set_figwidth(12)
-    for column, function in enumerate(PARAMS.values()):
-        for row, node in enumerate(function['nodes']):
+    for column, function in enumerate(FUNCTION_PARAMS.values()):
+        for row, node in enumerate(function['interpolation_nodes']):
             axs[column, row].set_title(
                 'Wielomiany interpolacyjne dla funkcji\n' rf"{function['function_title']} i siatki {node['node_title']}")
             axs[column, row].plot(arg, function['function'](arg), 'r', label='f(x)', linewidth=2.5)
-            for degree in node['polynomial_degree']:
+            for degree in node['polynomial_degrees']:
                 axs[column, row].plot(arg, interpolation(function['function'], node['function'], arg, degree),
                                       label=rf"$W_{'{' + str(degree) + '}'}(x)$")
             axs[column, row].legend(loc=8, prop={'size': 8})
@@ -55,4 +53,4 @@ def showPlot(arg):
 
 
 if __name__ == "__main__":
-    savePlot(X_NEW) if sys.argv[1] == 'save' else showPlot(X_NEW)
+    save_plots(X_RANGE) if sys.argv[1] == 'save' else show_plots(X_RANGE)
