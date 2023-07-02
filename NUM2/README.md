bbd5eac83bd3921c401e7dc76e7e71db630103f4
JKrecisz <jakub.krecisz@icloud.com>
2023-07-03 01:21:03 +0200

    Updated and improved NUM2

---
+++ NUM2/README.md
@@ -0,0 +1,64 @@
+# Linear System of Equations
+
+## Problem Description
+
+The given coefficient matrices are:
+
+**A1**:
+```
+ 2.34332898  -0.11253278  -0.01485349   0.33316649   0.71319625
+-0.11253278   1.67773628  -0.32678856  -0.31118836  -0.43342631
+-0.01485349  -0.32678856   2.66011353   0.85462464   0.16698798
+ 0.33316649  -0.31118836   0.85462464   1.54788582   0.32269197
+ 0.71319625  -0.43342631   0.16698798   0.32269197   3.27093538
+```
+
+**A2**:
+```
+ 2.34065520  -0.05353743   0.00237792   0.32944082   0.72776588
+-0.05353743   0.37604149  -0.70698859  -0.22898376  -0.75489595
+ 0.00237792  -0.70698859   2.54906441   0.87863502   0.07309288
+ 0.32944082  -0.22898376   0.87863502   1.54269444   0.34299341
+ 0.72776588  -0.75489595   0.07309288   0.34299341   3.19154447
+```
+
+
+And the given vectors are:
+
+**b** ≡ (3.55652063354463, −1.86337418741501, 5.84125684808554, −1.74587299057388, 0.84299677124244)<sup>T</sup>
+
+**b'** ≡ b + (10<sup>-5</sup>, 0, 0, 0, 0)<sup>T</sup>
+
+## Requirements
+
+- Python 3.x
+- NumPy
+
+## Usage
+
+Run the program using the command `make run`.
+
+## Solution
+
+The program uses the numpy library to solve the linear system of equations. It provides the following solutions:
+
+**A₁y₁ = b**:
+   - **y₁** = [solution vector]
+
+**A₁y₁' = b'**:
+   - **y₁'** = [solution vector]
+
+**A₂y₂ = b**:
+   - **y₂** = [solution vector]
+
+**A₂y₂' = b'**:
+   - **y₂'** = [solution vector]
+
+## Results and Interpretation
+
+The program calculates the differences between the solution vectors, given by **∆₁ = ||y₁ - y₁'||₂** and **∆₂ = ||y₂ - y₂'||₂**. It also computes the condition numbers of the coefficient matrices, given by **cond(A₁)** and **cond(A₂)**. Here are the results:
+
+- **∆₁** = [value of ∆₁]
+- **∆₂** = [value of ∆₂]
+- **cond(A₁)** = [condition number of A₁]
+- **cond(A₂)** = [condition number of A₂]
--- NUM2/main.py
+++ NUM2/main.py
@@ -3,15 +3,34 @@ import numpy as np
 from config import A1_MATRIX, A2_MATRIX, B_VECTOR, B_VECTOR_PRIM
 
 
-def solve_matrix(a, b):
-    return np.linalg.solve(a, b)
+def solve_matrix(matrix_a: np.ndarray[float], matrix_b: np.ndarray[float]) -> np.ndarray[float]:
+    """
+    Solves a linear system of equations for the given matrix A and vector B.
+
+    :param matrix_a: Coefficient matrix A.
+    :param matrix_b: Right-hand side vector B.
+    :return: Solution vector y.
+    """
+    return np.linalg.solve(matrix_a, matrix_b)
 
 
-def get_norm(matrix):
+def get_norm(matrix: np.ndarray[float]) -> float:
+    """
+    Computes the norm of the given matrix.
+
+    :param matrix: Input matrix.
+    :return: Norm of the matrix.
+    """
     return np.linalg.norm(matrix)
 
 
-def get_condition(matrix):
+def get_condition(matrix: np.ndarray[float]) -> float:
+    """
+    Computes the condition number of the given matrix.
+
+    :param matrix: Input matrix.
+    :return: Condition number of the matrix.
+    """
     return np.linalg.cond(matrix)
 
 
@@ -21,13 +40,17 @@ if __name__ == '__main__':
     y1_prim = solve_matrix(A1_MATRIX, B_VECTOR_PRIM)
     y2_prim = solve_matrix(A2_MATRIX, B_VECTOR_PRIM)
 
-    print(f"Solution for A\u2081y\u2081 = B:\n\ty\u2081 = {y1}\n")
-    print(f"Solution for A\u2081y\u2081' = B':\n\ty\u2081' = {y1_prim}\n")
-    print(f"Solution for A\u2082y\u2082 = B:\n\ty\u2082 = {y2}\n")
-    print(f"Solution for A\u2082y\u2082' = B':\n\ty\u2082' = {y2_prim}\n")
+    print(f"A₁y₁ = B:\n\ty₁ = {y1}\n")
+    print(f"A₁y₁' = B':\n\ty₁' = {y1_prim}\n")
+    print(f"A₂y₂ = B:\n\ty₂ = {y2}\n")
+    print(f"A₂y₂' = B':\n\ty₂' = {y2_prim}\n")
 
-    print("Δ\u2081 = ||y\u2081 - y\u2081'||\u2082\n\t Δ\u2081 = {:.20f}\n".format(get_norm(y1 - y1_prim)))
-    print("Δ\u2082 = ||y\u2082 - y\u2082'||\u2082\n\t Δ\u2082 = {:.20f}\n".format(get_norm(y2 - y2_prim)))
+    delta1 = get_norm(y1 - y1_prim)
+    delta2 = get_norm(y2 - y2_prim)
+    condition1 = get_condition(A1_MATRIX)
+    condition2 = get_condition(A2_MATRIX)
 
-    print("cond(A\u2081) = {:.20f}\n".format(get_condition(A1_MATRIX)))
-    print("cond(A\u2082) = {:.20f}\n".format(get_condition(A2_MATRIX)))
+    print(f"Δ₁ = ||y₁ - y₁'||₂\n\t Δ₁ = {delta1:.20f}\n")
+    print(f"Δ₂ = ||y₂ - y₂'||₂\n\t Δ₂ = {delta2:.20f}\n")
+    print(f"cond(A₁) = {condition1:.20f}\n")
+    print(f"cond(A₂) = {condition2:.20f}\n")
